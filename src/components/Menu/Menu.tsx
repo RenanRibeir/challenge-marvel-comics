9c7818b815efbed94f7333317e2cafd37dd13b7a
renanribeir <renanribeirow19@gmail.com>
2021-11-25 00:15:10 -0300

    feature: menu filter

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,15 @@
+import { Limit, Title } from "./style";
+
+const Menu =() => {
+    return (
+        <Title>
+            <Limit>
+            <span>Quadrinhos</span>
+            <span>Personagens</span>
+            <span>Eventos</span>
+            </Limit>
+        </Title>
+    );
+}
+
+export default Menu;
---
+++ src/components/Menu/style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Title = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80px;
+`
+
+export const Limit = styled.div`
+    display: flex;
+    justify-content: space-around;
+    min-width: 300px;
+    width: 50%;
+`
