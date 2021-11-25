6cc998b04ca516a8f76fc7f474b2b9fca8451c10
renanribeir <renanribeirow19@gmail.com>
2021-11-25 00:16:13 -0300

    feature: include menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import List from './components/List/List';
 import Logo from './components/Logo/Logo';
+import Menu from './components/Menu/Menu';
 
 const App:React.FC = () => { 
 
   return (
     <>
       <Logo/>
-      <List/>
+      <Menu/>
+      <List type={"events"}/>
     </>
     );
 };
