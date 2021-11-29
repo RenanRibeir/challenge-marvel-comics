67f35b5ac9c9690d5f916132a5ab4893163a075a
Renan Ribeiro <Renanribeirow19@gmail.com>
2021-11-29 03:22:46 -0300

    Style: global font

--- src/App.tsx
+++ src/App.tsx
@@ -6,43 +6,54 @@ import Logo from './components/Logo/Logo';
 import Menu from './components/Menu/Menu';
 import Modal from './components/Modal/Modal';
 import api from './services/api';
+import GlobalStyle from './styles/global';
 import { Container } from './styles/style';
 import { Generic } from './types';
 const App:React.FC = () => { 
 
   const [menu,setMenu] = useState<string>("comics");
+  const [item,setItem] = useState<Generic>({id: ' ',title:' ',thumbnail:{path:' ',extension:' '} });
  
   const updateName = (name: string):void => {
     setMenu(name);
     console.log(name);
   }
 
+  const updateItem = (item:Generic):void => {
+    setItem(item);
+    setModal(true);
+  }
+
   const [response,setResponse] = useState<Generic[]>([]);
   const [loading,setLoading] = useState<boolean>(true);
+  const [modal,setModal] = useState<boolean>(false);
 
     useEffect(() => {
       setLoading(true)
       api
-      .get(`/${menu}`)
+      .get(`/${menu}?offset=30`)
       .then(response => {
         setResponse(response.data.data.results);
       })
+      console.log(response);
       setLoading(false)
     },[menu]);
 
 
   return (
   <>
+    <GlobalStyle>
     <Container>
       <Logo/>
       <Menu updateState={updateName}/>
-      <Modal visible={true} data={response[5]}/>
+      <Modal visible={modal} data={item}/>
       {menu === "characters"?
-      <List response={response}/>
+      <List updateState={updateItem} response={response}/>
       :
-      <Grid response={response}/>}
+      <Grid updateState={updateItem} response={response}/>}
       <Loading show={loading}/>
     </Container>
+    </GlobalStyle>
   </>
     );
 };
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,5 +6,7 @@ export default createGlobalStyle`
         padding: 0;
         box-sinzing: border-box;
         outline: 0;
+        font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
     }
 `
